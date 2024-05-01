d0dd0559f36a8988e67f157d49c54d479b7d9ecc
Jerome Vilanova <jerome.vilanova@hotmail.fr>
2024-05-02 00:08:24 +0200

    Initializing the project and homepage creation

---
+++ react/src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import './Home.scss'
+
+function Home() {
+
+  return (
+    <div className='homeMainDiv'>
+    <div className='homeTitleBlock'>
+      <h1 className='homeMainTitle'>Econova</h1>
+    </div>
+    </div>
+  )
+}
+
+export default Home;
