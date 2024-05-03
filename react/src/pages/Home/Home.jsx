a3a6c876ec7454ced01822485e986ca9dc2e5eae
Jerome Vilanova <jerome.vilanova@hotmail.fr>
2024-05-03 20:24:49 +0200

    home page creation & deployment

--- react/src/pages/Home/Home.jsx
+++ react/src/pages/Home/Home.jsx
@@ -4,11 +4,17 @@ function Home() {
 
   return (
     <div className='homeMainDiv'>
-    <div className='homeTitleBlock'>
-    <div className='homeTitleCircle'>
-      <h1 className='homeMainTitle'>Econova</h1>
-    </div>
-    </div>
+      <div className='line'></div>
+        <div className='homeTitleBlock'>
+          <div className='homeTitleCircle'>
+           <h1 className='homeMainTitle'>Econova</h1>
+           <p className='homeMainSubTitle'>production</p>
+          </div>
+        </div>
+        <div className='homeTextBlock'>
+          <p className='homeText'>Préparez-vous !<br/><br/> Le site de la chaîne Econova arrive bientôt.</p> 
+        </div>
+        <div className='line'></div>
     </div>
   )
 }
