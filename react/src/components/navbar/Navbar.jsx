353bfb834847d926b781f9b90d4bb4ca77b6a37a
Jerome Vilanova <jerome.vilanova@hotmail.fr>
2024-06-21 18:54:54 +0200

    Creation menu burger

--- react/src/components/navbar/Navbar.jsx
+++ react/src/components/navbar/Navbar.jsx
@@ -1,23 +1,44 @@
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Logo from '../logo/Logo';
 import hut from "../../assets/icons/home.png";
-import './Navbar.scss'
+import './Navbar.scss';
 
-function Navbar() {
+export default function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   return (
-    <div className='navbarDiv'>
-        <Logo/>
-        <div class="navbarLinksBlock">
-        <Link class="links" to="/understand"><h2 class="navbartitleLinks">Comprendre</h2></Link>
-        <Link class="links" to="/adapt"><h2 class="navbartitleLinks">S'adapter</h2></Link>
-        <Link class="links" to="/act"><h2 class="navbartitleLinks">Agir</h2></Link>
-        <Link class="links" to="/adhere"><h2 class="navbartitleLinks">Adhérer</h2></Link>
-        <Link class="links" to="/contact"><h2 class="navbartitleLinks">Contact</h2></Link>
-        <Link class="links" to="/"> <img src={hut} alt="home" /></Link>
-        </div>
-    </div>
-  )
+    <nav className="navBar">
+      <Logo />
+      <button type="button" className="burger-menu" onClick={toggleMenu}>
+        <span className={`bar ${isMenuOpen ? "open" : ""}`} />
+        <span className={`bar ${isMenuOpen ? "open" : ""}`} />
+        <span className={`bar ${isMenuOpen ? "open" : ""}`} />
+      </button>
+      <ul className={`navbarLinksBlock ${isMenuOpen ? "open" : ""}`}>
+        <li className="links">
+          <Link to="/understand"><h2 className="navbartitleLinks">Comprendre</h2></Link>
+        </li>
+        <li className="links">
+          <Link to="/adapt"><h2 className="navbartitleLinks">S'adapter</h2></Link>
+        </li>
+        <li className="links">
+          <Link to="/act"><h2 className="navbartitleLinks">Agir</h2></Link>
+        </li>
+        <li className="links">
+          <Link to="/adhere"><h2 className="navbartitleLinks">Adhérer</h2></Link>
+        </li>
+        <li className="links">
+          <Link to="/contact"><h2 className="navbartitleLinks">Contact</h2></Link>
+        </li>
+        <li className="links">
+          <Link to="/"> <img src={hut} alt="home" /></Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
-
-export default Navbar;
