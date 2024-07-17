a1d692abe9503b8f5e46ef688188078f5e3f018a
Jerome Vilanova <jerome.vilanova@hotmail.fr>
2024-07-17 13:55:52 +0200

    Creation of the 'Understand' page content

--- react/src/pages/Home/Home.jsx
+++ react/src/pages/Home/Home.jsx
@@ -9,11 +9,6 @@ function Home() {
       <Navbar/>
         <div className='homeTextBlock'>
         <h1 className='homeTitle'>Sur les chemins de la terre</h1>
-        <h1 className='homeTitle2'>Sur les chemins de la terre</h1>
-        <h1 className='homeTitle3'>Sur les chemins de la terre</h1>
-        <h1 className='homeTitle4'>Sur les chemins de la terre</h1>
-        <h1 className='homeTitle5'>Sur les chemins de la terre</h1>
-        <h1 className='homeTitle6'>Sur les chemins de la terre</h1>
          <p class="homeText">Bienvenu sur le site d’ECONOVA PRODUCTION,<br/> le média international engagé sur les sujets environnementaux et sociétaux.</p>
         </div>
         <Footer/>
